d02f272ce977f2e88907d604cf658869fd59be92
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-08-25 11:09:32 -0300

    exercicio: parte 7 e 8

---
+++ aula-node-22.4-exercicio/middlewares/validateCreatedAt.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+    const { createdAt } = req.body.description;
+    const isFormatDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+
+    if (!isFormatDate.test(createdAt)) {
+        res.status(400).json({ message: 'O campo createdAt deve ter o formato \'dd/mm/aaaa\'' });
+    }
+    next();
+};
--- aula-node-22.4-exercicio/src/app.js
+++ aula-node-22.4-exercicio/src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const validatePrice = require('../middlewares/validatePrice');
 const validateName = require('../middlewares/validateName');
+const validateDescription = require('../middlewares/validateDescription');
+const validateCreatedAt = require('../middlewares/validateCreatedAt');
+const validateRating = require('../middlewares/validateRating');
 
 const app = express();
 
 app.use(express.json()); // mw criado pela comunidade p/ interpretar conteúdo json
-app.post('/activities', validatePrice, validateName, (_req, res) => {
+app.post('/activities',
+    validatePrice,
+    validateName,
+    validateDescription,
+    validateCreatedAt,
+    validateRating,
+    (_req, res) => {
     res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
 
