ada4ed83b994159a3da8b855566b3ac7bb1bf45d
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-08-25 11:30:30 -0300

    exercicio2: parte 1

--- aula-node-22.4-exercicio/src/app.js
+++ aula-node-22.4-exercicio/src/app.js
@@ -5,6 +5,7 @@ const validateDescription = require('../middlewares/validateDescription');
 const validateCreatedAt = require('../middlewares/validateCreatedAt');
 const validateRating = require('../middlewares/validateRating');
 const validateDifficulty = require('../middlewares/validateDifficulty');
+const generateToken = require('../utils/generateToken');
 
 const app = express();
 
@@ -18,6 +19,17 @@ app.post('/activities',
     validateDifficulty,
     (_req, res) => {
     res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
+});
+
+app.post('/signup', (req, res) => {
+    const { email, password, firstName, phone } = req.body;
+
+    if ([email, password, firstName, phone].includes(undefined)) {
+        return res.status(401).json({ message: 'Campos ausentes' });
+    }
+
+    const token = generateToken();
+    return res.status(200).json({ token });
 });
 
 module.exports = app;
